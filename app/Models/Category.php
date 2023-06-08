c478f20df4bbcbe5b73880bf1d509d8c8b2f32df
marekgacek45 <marekgacek45@gmail.com>
2023-06-08 07:30:59 +0200

    create models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\News;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function newses(){
+        return $this->hasMany(News::class);
+    }
 }
