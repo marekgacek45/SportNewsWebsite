4eedb411b99d9f546e15d4c455b53e51ce4566d2
marekgacek45 <marekgacek45@gmail.com>
2023-06-10 14:20:31 +0200

    finish main page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,24 +14,25 @@ class PagesController extends Controller
         $basketball_newses = News::with('category')->where('category_id', '=', '2')->get();
         $hockey_newses = News::with('category')->where('category_id', '=', '3')->get();
         $other_newses = News::with('category')
-    ->whereNotIn('category_id', [1, 2, 3])
-    ->get();
+            ->whereNotIn('category_id', [1, 2, 3])
+            ->get();
 
         $newses = News::with('category')->orderBy('id', 'desc')->get();
         $lastNews = $newses->first();
         $newses->shift();
-        return view('pages/home', ['newses' => $newses, 'lastNews' => $lastNews,'football_newses'=>$football_newses,'basketball_newses'=>$basketball_newses,'hockey_newses'=>$hockey_newses,'other_newses'=>$other_newses]);
+        return view('pages/home', ['newses' => $newses, 'lastNews' => $lastNews,  'football_newses' => $football_newses, 'basketball_newses' => $basketball_newses, 'hockey_newses' => $hockey_newses, 'other_newses' => $other_newses]);
     }
     public function news($id)
     {
         $newses = News::with('category')->orderBy('id', 'desc')->get();
         $news = News::with('category')->findOrFail($id);
-        return view('pages/news', ['news' => $news,'newses' => $newses,]);
+        return view('pages/news', ['news' => $news, 'newses' => $newses,]);
     }
 
-    public function category($id){
+    public function category($id)
+    {
         $newses = News::with('category')->where('category_id', '=', $id)->get();
 
-        return(view('pages/category',['newses'=>$newses]));
+        return (view('pages/category', ['newses' => $newses]));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,102 +4,132 @@
 
 
     <main>
-        <div  class="news_bgc" style="background-image: url({{asset('storage/news_bgc.jpg')}})">
+        <div class="news_bgc" style="background-image: url({{ asset('storage/news_bgc.jpg') }})">
             <div class="news_bgc-opacity"></div>
-            <div >
-            <section class="home section container">
-                
-                <div class="home__latest">
-                    <a href="{{ route('news', $lastNews->id) }}">
-                        <div class="home__latest-last"
-                            style="background-image: url('{{ asset('storage/' . $lastNews->image) }}');">
-                            <div class="home__latest-last--opacity"></div>
-                            <div class="home__latest-last-text">
-                                <h3>
-                                    {{ $lastNews->title }}
-                                </h3>
-                                <p>
-                                    {{ $lastNews->description }}
-                                </p>
-                            </div>
+            <div>
+                <section class="home section container">
 
-                    </a>
+                    <div class="home__latest">
+                        <a href="{{ route('news', $lastNews->id) }}">
+                            <div class="home__latest-last"
+                                style="background-image: url('{{ asset('storage/' . $lastNews->image) }}');">
+                                <div class="home__latest-last--opacity"></div>
+                                <div class="home__latest-last-text">
+                                    <h3>
+                                        {{ $lastNews->title }}
+                                    </h3>
+                                    <p>
+                                        {{ $lastNews->description }}
+                                    </p>
+                                </div>
+
+                        </a>
+                    </div>
+                    <div class="home__latest-rest">
+                        @php $i = 0 @endphp
+                        @foreach ($newses as $news)
+                            <a href="{{ route('news', $news->id) }}">
+                                <div class="home__latest-rest-card">
+                                    <div class="home__latest-rest-card-text">
+                                        <h3>
+                                            {{ $news->title }}
+                                        </h3>
+
+                                    </div>
+                                    <img src="{{ asset('storage/' . $news->image) }}" alt="">
+                                </div>
+                            </a>
+                            @php $i++ @endphp
+                            @if ($i === 3)
+                            @break
+                        @endif
+                    @endforeach
                 </div>
-                <div class="home__latest-rest">
-                    <?php foreach ($newses as $news): ?>
-                    <a href="{{ route('news', $news->id) }}">
-                        <div class="home__latest-rest-card">
-                            <div class="home__latest-rest-card-text">
-                                <h3>
-                                    {{ $news->title }}
-                                </h3>
-
-                            </div>
-                            <img src="{{ asset('storage/' . $news->image) }}" alt="">
-                        </div>
-                    </a>
-                    <?php endforeach ?>
-                </div>
         </div>
-        </div>
-        </section>
-        </div>
+    </div>
+    </section>
+    </div>
 
-        <div class="heroImg" style="background-image: url('{{asset('storage/wesley-tingey-dKCKiC0BQtU-unsplash.jpg' )}}')"></div>
 
-        <section class="section newses_categories" >
-            <h3>Piłka Nożna</h3>
-            <div class="newses-categories__container container" style="background-color: ">
 
-                
-                @foreach ($football_newses as $football_news)
+    <section class="section newses_categories">
+        <h3>Piłka Nożna</h3>
+        <div class="newses-categories__container container">
+
+
+            @php $j = 0 @endphp
+            @foreach ($football_newses as $football_news)
                 <div class="newses-categories__card">
-                    <a href="{{route('news',$football_news->id)}}">
-                    <img src="{{asset('storage/' . $football_news->image)  }}" alt="">
-                    <h5>{{ $football_news->title }}</h5>
-                </a>
+                    <a href="{{ route('news', $football_news->id) }}">
+                        <img src="{{ asset('storage/' . $football_news->image) }}" alt="">
+                        <h5>{{ $football_news->title }}</h5>
+                    </a>
                 </div>
-                @endforeach
-            </div>
-            <a href="{{ route('category', $football_news->category_id) }}">Więcej wiadomości z tej kategorii</a>
-        </section>
+                @php $j++ @endphp
+                @if ($j === 4)
+                @break
+            @endif
+            @endforeach
+        </div>
+        <a href="{{ route('category', $football_news->category_id) }}">Więcej wiadomości z tej kategorii</a>
+    </section>
 
-<div class="heroImg" style="background-image: url('{{asset('storage/richard-bagan-SmQ2Cku3alc-unsplash.jpg' )}}')"></div>
+    <div class="heroImg"
+        style="background-image: url('{{ asset('storage/richard-bagan-SmQ2Cku3alc-unsplash.jpg') }}')"></div>
 
-        <section class="section newses_categories container" >
+        <section class="section newses_categories">
             <h3>Koszykówka</h3>
-            <div class="newses-categories__container">
-                @foreach ($basketball_newses as $basketball_news)
-                <div class="newses-categories__card">
-                    <a href="{{route('news',$basketball_news->id)}}">
-                    <img src="{{asset('storage/' . $basketball_news->image)  }}" alt="">
-                    <p>{{ $basketball_news->title }}</p>
-                </a>
-                </div>
-                @endforeach
-            </div>
-            <a href="{{ route('category', $football_news->category_id) }}">Więcej wiadomości z tej kategorii</a>
-        </section>
-        <div class="heroImg" style="background-image: url('{{asset('storage/jerry-yu-HRo2pfbVulQ-unsplash.jpg' )}}')"></div>
-        <section class="section newses_categories" >
-            <h3>Hokej</h3>
             <div class="newses-categories__container container">
-                @foreach ($hockey_newses as $hockey_news)
-                <div class="newses-categories__card">
-                    <a href="{{route('news',$hockey_news->id)}}">
-                    <img src="{{asset('storage/' . $hockey_news->image)  }}" alt="">
-                    <p>{{ $hockey_news->title }}</p>
-                </a>
-                </div>
+    
+    
+                @php $j = 0 @endphp
+                @foreach ($basketball_newses as $basketball_news)
+                    <div class="newses-categories__card">
+                        <a href="{{ route('news', $basketball_news->id) }}">
+                            <img src="{{ asset('storage/' . $basketball_news->image) }}" alt="">
+                            <h5>{{ $basketball_news->title }}</h5>
+                        </a>
+                    </div>
+                    @php $j++ @endphp
+                    @if ($j === 4)
+                    @break
+                @endif
                 @endforeach
             </div>
-            <a href="{{ route('category', $hockey_news->category_id) }}">Więcej wiadomości z tej kategorii</a>
+            <a href="{{ route('category', $basketball_news->category_id) }}">Więcej wiadomości z tej kategorii</a>
         </section>
 
-        <div class="heroImg" style="background-image: url('{{asset('storage/andrea-leopardi-QVD3Xht9txA-unsplash.jpg' )}}')"></div>
 
-         
-    </main>
+
+    <div class="heroImg" style="background-image: url('{{ asset('storage/jerry-yu-HRo2pfbVulQ-unsplash.jpg') }}')">
+    </div>
+
+    <section class="section newses_categories">
+        <h3>Hokej</h3>
+        <div class="newses-categories__container container">
+
+
+            @php $j = 0 @endphp
+            @foreach ($hockey_newses as $hockey_news)
+                <div class="newses-categories__card">
+                    <a href="{{ route('news', $hockey_news->id) }}">
+                        <img src="{{ asset('storage/' . $hockey_news->image) }}" alt="">
+                        <h5>{{ $hockey_news->title }}</h5>
+                    </a>
+                </div>
+                @php $j++ @endphp
+                @if ($j === 4)
+                @break
+            @endif
+            @endforeach
+        </div>
+        <a href="{{ route('category', $hockey_news->category_id) }}">Więcej wiadomości z tej kategorii</a>
+    </section>
+
+
+
+
+</main>
 
 
 
